7afd435715f70770cf02a70f7cda50adba5d693d
Yashraj Patil <yashrajspatil808@gmail.com>
2026-04-21 21:20:08 +0530

    Update 01Linkedlist_Easy.cpp

--- Linked_List/01Linkedlist_Easy.cpp
+++ Linked_List/01Linkedlist_Easy.cpp
@@ -8,7 +8,7 @@ public:
     int data;
     Node* next;
 
-    //constructor to assign the values in created node
+    //constructor to assign the values in created node........
     Node(int data1, Node* next1){
         data = data1;
         next = next1;

182c976d745a2277186c0673005798e0ddf79a21
Yashraj Patil <yashrajspatil808@gmail.com>
2026-06-23 19:01:32 +0530

    add binary search

---
+++ Arrays/09binarySearch.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+
+        while(low <= high){
+            // int mid = low - (low-high/2);
+            int mid = (low + high)/2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid]<target) {low = mid+1; }
+
+            else high = mid-1;
+        }
+        return -1;
+    }
+};

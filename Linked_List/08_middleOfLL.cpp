482d8f8bc8569e9b9e852cafaa8d4b11e6d44ba5
Yashraj Patil <yashrajspatil808@gmail.com>
2026-07-22 18:47:13 +0530

    Solved middle of the LL problem

--- Linked_List/08_middleOfLL.cpp
+++ Linked_List/08_middleOfLL.cpp
@@ -9,20 +9,46 @@ using namespace std;
       ListNode(int x) : val(x), next(nullptr) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
+
+//BRUTE SOLUTION - Count the length and return length/2 number element!
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* curr = head;
+        //counting the elements in LL
+        int count = 0;
+        while(curr != NULL){
+            count++;
+            curr = curr->next;
+        }
+
+        curr = head;
+        //finding the mid node
+        count = (count)/2;
+        while(count != 0){
+            curr = curr->next;
+            count--;
+        }
+        return curr;
+
+    }
+};
  
+//OPTIMAL SOUTION - Tortoise and Hair algorithm!
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         if(head == NULL || head->next == NULL) return head;
 
-        ListNode* slow = head;
+        //Tortoise-Hair algorithm - fast and slow pointer approach
         ListNode* fast = head;
-        //Tortoise-hair algorithm
+        ListNode* slow = head;
         while(fast != NULL && fast->next != NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
         return slow;
-
     }
 };
